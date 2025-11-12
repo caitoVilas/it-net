8370bbe2b224a0ddbe44e46566c4fd351c355177
caito <caitocd@gmail.com>
2025-11-12 10:59:58 -0300

    initilal conmit

---
+++ it-net-be/src/main/java/com/itnet/ItNetBeApplication.java
@@ -0,0 +1,13 @@
+package com.itnet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItNetBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItNetBeApplication.class, args);
+    }
+
+}
---
+++ it-net-be/src/test/java/com/itnet/ItNetBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itnet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItNetBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
